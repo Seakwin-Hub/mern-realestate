3abdd190bdf16092a838d6da9bf11ad8029dd986
Seakwin-Hub <dewinner82@gmail.com>
2024-06-21 00:48:54 +0700

    Create middleware and error function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -10,9 +10,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/user", userRoute);
-app.use("/api/auth", authRoute);
-
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -24,4 +21,20 @@ mongoose
 
 app.listen(3000, () => {
   console.log("Server is Running on Port : 3000");
+});
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+
+//err : current error
+//next : next middleware
+//500: internal server error
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
