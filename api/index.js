09e35229234eda8be80710163256c1e8a3f4d6cc
Seakwin-Hub <dewinner82@gmail.com>
2024-06-20 23:21:13 +0700

    Connect to Database MongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Mongo DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Server is Running on Port : 3000");
